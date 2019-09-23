6ce79b9bbb7c09002c8bf7fe4e913816c7b286d4
Olly Headey <olly@headey.org>
2019-09-23 17:12:34 +0100

    Revert "Merge branch 'no-coffeescript'"
    This reverts commit 8735b6c343a6ed0b50929e41f376968618fd1e9f, reversing
    changes made to 24ec11febec66b3dd71a3881d9be1ee45d1664b8.

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module Freebay
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.generators.javascript_engine
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
