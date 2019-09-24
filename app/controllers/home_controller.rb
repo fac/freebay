2199b148d57bfd158f1b626626f556b3a07f7e07
Hamish Deas <hamish.deas@freeagent.com>
2019-09-24 14:14:36 +0100

    Add basic filter to controller and index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   before_action :require_login
 
   def index
-    @listings = Listing.active.order(:end_time).all
+    if params[:condition]
+      @listings = Listing.where(condition: params[:condition]).active.order(:end_time).all
+    else
+      @listings = Listing.active.order(:end_time).all
+    end
   end
 end
