1bdf5787e268c79716b75233db5c8268df768fae
Peter Singh <singh.prd@gmail.com>
2019-09-25 16:59:52 +0100

    Add end time to listing show page

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,13 @@
 module ListingsHelper
+  def humanized_end_time(listing)
+    if listing.active?
+      if listing.end_time > Time.now
+        "Ends in #{distance_of_time_in_words(Time.now, listing.end_time)}"
+      else
+        "Ended"
+      end
+    else
+      "Not Active"
+    end
+  end
 end
